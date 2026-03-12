48c97ff95ef982899c5ac15d63c1f6e043f6434e
Thibault Grandemenge <160125763+Grand-Thibault@users.noreply.github.com>
2026-03-12 09:48:17 +0100

    updated version in kitsData.js

--- data/kitsData.js
+++ data/kitsData.js
@@ -364,8 +364,8 @@ export const kitsData = {
       description: 'Trace parts and materials across the entire value chain to enable data driven use cases over all n-tier levels.',
       metadata: {
         created: '2023-04-12',
-        lastUpdated: '2025-08-07',
-        latestVersion: '7.0.0',
+        lastUpdated: '2026-03-12',
+        latestVersion: '8.0.0',
         new: false
       }
     },
